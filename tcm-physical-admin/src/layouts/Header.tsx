6a7f850969791e4266d130f42f49a7c58777998c
a-frogfrog <a_frogfrog@outlook.com>
2025-08-26 18:54:44 +0800

    🙋‍♂️feat:新增字体inter,Layout新增TopActionBar组件.

--- tcm-physical-admin/src/layouts/Header.tsx
+++ tcm-physical-admin/src/layouts/Header.tsx
@@ -8,30 +8,48 @@ import {
 } from '#/components/ui/breadcrumb';
 import { Separator } from '#/components/ui/separator';
 import { SidebarTrigger } from '#/components/ui/sidebar';
+import { Switch } from '#/components/ui/switch';
+import { Bell, Github, Maximize } from 'lucide-react';
+
+const BreadcrumbUI = () => {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem className='hidden md:block'>
+          <BreadcrumbLink href='#'>仪表板</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator className='hidden md:block' />
+        <BreadcrumbItem>
+          <BreadcrumbPage>工作台</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  );
+};
+
+const TopActionBar = () => {
+  return (
+    <nav className='flex items-center gap-4'>
+      <Switch />
+      <Bell strokeWidth={1.75} className='cursor-pointer' />
+      <Maximize strokeWidth={1.75} className='cursor-pointer' />
+      <Github strokeWidth={1.75} className='cursor-pointer' />
+    </nav>
+  );
+};
 
 export default function Header() {
   return (
-    <header className='flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
-      <div className='flex items-center gap-2 px-4'>
+    <header className='flex px-4 h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12'>
+      <div className='flex flex-1 items-center gap-2 px-4'>
         <SidebarTrigger className='-ml-1' />
         <Separator
           orientation='vertical'
           className='mr-2 data-[orientation=vertical]:h-4'
         />
-        <Breadcrumb>
-          <BreadcrumbList>
-            <BreadcrumbItem className='hidden md:block'>
-              <BreadcrumbLink href='#'>
-                Building Your Application
-              </BreadcrumbLink>
-            </BreadcrumbItem>
-            <BreadcrumbSeparator className='hidden md:block' />
-            <BreadcrumbItem>
-              <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <BreadcrumbUI />
       </div>
+      <TopActionBar />
     </header>
   );
 }
