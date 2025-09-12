29de8e42a045e024e6eb5a1f0dd4b8361495b309
a-frogfrog <a_frogfrog@outlook.com>
2025-09-12 16:45:36 +0800

    🧂 refactor:移除为完成的排班组件.

--- tcm-physical-admin/src/pages/schedule/arrange/index.tsx
+++ tcm-physical-admin/src/pages/schedule/arrange/index.tsx
@@ -1,11 +1,10 @@
 import { PageTitle } from '#/components/common';
-import Calendar from './modules/Calendar';
+// import Calendar from './modules/Calendar';
 
 export default function ScheduleArrangePage() {
   return (
     <>
       <PageTitle title='排班管理' desc='安排技术人员的服务排班计划' />
-      <Calendar className='base-card my-6' />
     </>
   );
 }
