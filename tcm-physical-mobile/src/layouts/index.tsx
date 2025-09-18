54cc82f6a57e3b4ac86e60a273577e1ed3064ed6
a-frogfrog <a_frogfrog@outlook.com>
2025-09-18 08:47:02 +0800

    🏸 feat：新增icon.   - 新增 favicon 用于显示浏览器标签页.

--- tcm-physical-mobile/src/layouts/index.tsx
+++ tcm-physical-mobile/src/layouts/index.tsx
@@ -1,12 +1,15 @@
 import { Outlet } from 'react-router-dom';
 import TabBar from './components/TabBar';
 import { tabs } from './constants';
+import { useApplication } from '#/hooks';
+
 export default function Layout() {
+  const { name } = useApplication();
   return (
     <>
       <header>
         <h1 className='p-2 px-4 font-sans text-xl font-bold text-[#008236]'>
-          御合堂
+          {name}
         </h1>
       </header>
       <main className='scroll-bar-hide h-[calc(100vh-80px)] overflow-auto'>
