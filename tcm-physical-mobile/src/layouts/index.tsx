151abdc3f46e0a773b5f9f427740d4782b05aaef
a-frogfrog <a_frogfrog@outlook.com>
2025-09-19 11:56:15 +0800

    🛞 feat：新增footer组件.  - 优化部分页面样式.

--- tcm-physical-mobile/src/layouts/index.tsx
+++ tcm-physical-mobile/src/layouts/index.tsx
@@ -1,20 +1,36 @@
 import { Outlet } from 'react-router-dom';
 import TabBar from './components/TabBar';
 import { tabs } from './constants';
+import { useApplication } from '#/hooks';
 
 export default function Layout() {
   return (
-    <>
-      <main className='scroll-bar-hide h-[calc(100vh-80px)] overflow-auto'>
+    <div className='scroll-bar-hide v h-[calc(100vh-50px)] overflow-auto'>
+      <main>
         <Outlet />
       </main>
-      <footer className='fixed right-0 -bottom-1 left-0 py-2'>
+      <footer className=''>
+        <AppFooter />
         <AppTabBar />
       </footer>
-    </>
+    </div>
   );
 }
 
 const AppTabBar = () => {
   return <TabBar tabs={tabs} />;
+};
+
+const AppFooter = () => {
+  const { name } = useApplication();
+  return (
+    <div className='mt-8 bg-blue-50 py-4'>
+      <div className='container mx-auto px-4 text-center text-sm text-gray-600'>
+        <p>
+          © {new Date().getFullYear()} {name}. 保留所有权利
+        </p>
+        <p className='mt-1'>客服电话：400-123-4567</p>
+      </div>
+    </div>
+  );
 };
