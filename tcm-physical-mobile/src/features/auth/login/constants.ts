ad401da78b1e81f4677fcdce0928abb18de55417
a-frogfrog <a_frogfrog@outlook.com>
2025-09-25 14:13:27 +0800

    🔗 refactor(auth)：移除auth/login constants文件.

--- tcm-physical-mobile/src/features/auth/login/constants.ts
+++
@@ -1,8 +0,0 @@
-import { z } from 'zod';
-
-export const formSchema = z.object({
-  email: z.email('请输入正确的邮箱格式'),
-  password: z.string().min(6, '密码最少6个字符').max(32),
-});
-
-export type FormSchema = z.infer<typeof formSchema>;
