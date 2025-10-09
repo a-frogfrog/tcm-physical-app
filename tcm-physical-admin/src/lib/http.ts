87dd3b37342af2d70ab4761b7f8f68c77a078932
a-frogfrog <a_frogfrog@outlook.com>
2025-10-09 17:10:09 +0800

    🍵 feat(request): 提取为公共库.  - 将封装的axios提取为公共库,上传npm,并添加为项目依赖.  - 移除product table 的模拟数据.

---
+++ tcm-physical-admin/src/lib/http.ts
@@ -0,0 +1,47 @@
+import { createAxiosRequest } from 'frog-request';
+
+import type { AxiosError } from 'axios';
+import { toast } from 'sonner';
+
+export const http = createAxiosRequest({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  handleRequestSuccess: (config) => {
+    // config.headers['Authorization'] = `Bearer ${useAuthStore.getState().token}`;
+    return config;
+  },
+  handleRequestError: (error) => {
+    return Promise.reject(error.response?.data);
+  },
+  handleResponseSuccess: (response) => {
+    const data = response.data;
+    return data;
+  },
+  handleResponseError: (error) => {
+    const { code } = error;
+
+    const responseError = RESPONSE_ERROR[code as keyof typeof RESPONSE_ERROR];
+    if (responseError) {
+      responseError.task(error);
+    }
+    return Promise.reject(error.response?.data);
+  },
+});
+
+/**
+ * @description 响应错误处理
+ */
+const RESPONSE_ERROR = {
+  ERR_NETWORK: {
+    task: (_error: AxiosError) => {
+      console.log(_error);
+      // window.location.href = routes.auth.login.path;
+      toast.error('网络错误，请稍后重试！ 😵');
+    },
+  },
+  401: {
+    //eslint-disable-next-line @typescript-eslint/no-unused-vars
+    task: (_error: AxiosError) => {
+      toast.error('登录过期，请重新登录！ 🔑');
+    },
+  },
+};
